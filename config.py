2eb79e3d68964ecd3cb2bd642eadc958ddc2d4e8
вашÐxqwift <qwproton@gmail.com>
2024-09-16 19:42:06 +0300

    Fix crash code

--- config.py
+++
@@ -1,8 +0,0 @@
-import dotenv
-import os
-
-dotenv.load_dotenv()
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-ADMIN_URL = os.getenv('ADMIN_URL')
-ADMIN_USERNAME = os.getenv('ADMIN_USERNAME')
-FILE_PATH = os.getenv('FILE_PATH')
